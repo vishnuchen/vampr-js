4b8d7350fd30773b84b213b61ffba9977e68a2ec
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-30 17:06:30 +0000

    vampire

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberAway = 0;
 
+    if (this.creator)
+    {
+      return 1 + this.creator.numberOfVampiresFromOriginal;
+    }
+    else return 0;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal)
+    {
+      return true;
+    }
+    else return false;
   }
 
   /** Stretch **/
@@ -41,4 +52,3 @@ class Vampire {
 }
 
 module.exports = Vampire;
-
